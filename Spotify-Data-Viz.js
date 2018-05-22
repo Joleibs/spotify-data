adc9eb5f8c8d53d37b959dfdefc608f56fd173d7
Josh <josh.leibsohn@gmail.com>
2018-05-21 18:16:12 -0700

    ay

--- Spotify-Data-Viz.js
+++ Spotify-Data-Viz.js
@@ -23,7 +23,7 @@ function setup() {
 function draw (dance, acoustic, valence, liveness, speechiness) {
 				// rect(50, 50, rectLength, rectHeight); 
         text('Danceability:' + dance, 50, 50);
-        text('Acousticness:' + acoustic, 50, 50);
+        text('Acousticness:' + acoustic, 50, 80);
         text('Valence:' + valence, 50, 110);
         text('Liveness:' + liveness, 50, 140);
         text('Speechiness:' + speechiness, 50, 170);
