65c8ad8ab54dd268fd6c18c0d866d7a11e7b9400
Josh <josh.leibsohn@gmail.com>
2018-05-23 21:03:54 -0700

    test

--- charts.js
+++ charts.js
@@ -6,30 +6,26 @@ var donut = (function(one){
   var radius = 200;
   var greyColor = '#e8e8e8';
   var dataColor = '#1dafd3';
-  var colors = d3.scaleOrdinal([greyColor, dataColor]);
-
-  var piedata = [{name: "one", value: 50}, {name: "two", value: 30}];
-
-  var arc = d3.arc().innerRadius(radius - 100).outerRadius(radius);
-
+  var red
+  var colors = d3.scaleOrdinal([dataColor, greyColor]);
+  var piedata = [{name: "one", value: 20}, {name: "two", value: 130}];
+  var arc = d3.arc().innerRadius(radius - 50).outerRadius(radius);
   var donutChart = d3.select('#danceDonut').append('svg')
     .attr('width', width)
     .attr('height', height)
     .append('g')
     .attr('transform', 'translate(' + (width - radius) + ',' + (height - radius) + ')');
-  
   var pie = d3.pie()
     .sort(null)
     .value(function(piedata) { return piedata.value; });
-
-donutChart.d3.select('svg g').selectAll('path').data(pie(piedata))
+    
+  var arc_g = d3.select('svg g').selectAll('arc').data(pie(piedata))
     .enter().append('g')
-    .attr('class', 'slice').
-    selectAll('.slice')
-    .append('path')
+    .attr('class', 'slice');
+  arc_g.append('path')
+    .attr("d",arc)
     .attr('fill', function(d, range) {
-    return colors(range);
-     })
- 
-      .attr('d', arc)
-})();
+     return colors(range);
+  })
+ })();
+
