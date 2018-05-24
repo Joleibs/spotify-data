93bcdf565331d9e7c062fa92bd387d4bfd8372f0
Josh <josh.leibsohn@gmail.com>
2018-05-23 20:39:48 -0700

    test

--- charts.js
+++ charts.js
@@ -7,13 +7,17 @@ var donut = (function(one){
   var greyColor = '#e8e8e8';
   var dataColor = '#1dafd3';
   var colors = d3.scaleOrdinal([greyColor, dataColor]);
+  
   var piedata = [{name: "one", value: 50}, {name: "two", value: 30}];
+
   var arc = d3.arc().innerRadius(radius - 100).outerRadius(radius);
+
   var donutChart = d3.select('#danceDonut').append('svg')
     .attr('width', width)
     .attr('height', height)
     .append('g')
     .attr('transform', 'translate(' + (width - radius) + ',' + (height - radius) + ')');
+  
   var pie = d3.pie()
     .sort(null)
     .value(function(piedata) { return piedata.value; });
@@ -21,11 +25,11 @@ var donut = (function(one){
   d3.select('svg g').selectAll('arc').data(pie(piedata))
     .enter().append('g')
     .attr('class', 'slice');
-    // var slices1 = d3.selectAll('g.slice')
-    // .append('path')
-    // .attr('fill', function(d, range) {
-    // return colors(range);
-    //  })
+    var slices1 = d3.selectAll('g.slice')
+    .append('path')
+    .attr('fill', function(d, range) {
+    return colors(range);
+     })
  
       // .attr('d', arc)
 })();
