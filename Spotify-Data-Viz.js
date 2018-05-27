b4280569f97c6d12fbe925a87a3ef5f5d8dbad61
Josh <josh.leibsohn@gmail.com>
2018-05-27 00:30:31 -0700

    bec

--- Spotify-Data-Viz.js
+++ Spotify-Data-Viz.js
@@ -382,8 +382,8 @@ function visualize () {
 					writeText ("speechData", "Speechiness: " + songData.audio_features[0].speechiness); 
 					writeText ("liveData", "Liveness: " + songData.audio_features[0].liveness); 
 					writeText ("valenceData", "Valence: " + songData.audio_features[0].valence); 
-					writeText ("loudData", songData.audio_features[0].loudness + " lufs"); 
-					writeText ("tempoData", songData.audio_features[0].tempo + " bpm"); 
+					writeText ("loudData", "loudness: " + songData.audio_features[0].loudness + " lufs"); 
+					writeText ("tempoData", "Tempo: " + songData.audio_features[0].tempo + " bpm"); 
 					writeText ("keyData", "key:" + songData.audio_features[0].key); 
 
 					donutChart1 ('#danceDonut', songData.audio_features[0].danceability, "#009999"); 
