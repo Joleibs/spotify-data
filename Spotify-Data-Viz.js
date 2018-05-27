4a78255254e86581938c7d4e01acd381e48112e3
Josh <josh.leibsohn@gmail.com>
2018-05-27 09:58:31 -0700

    w/ out text debug

--- Spotify-Data-Viz.js
+++ Spotify-Data-Viz.js
@@ -376,16 +376,16 @@ function visualize () {
     		var hasRun = 1;
     		console.log(songData);
     		
-   			writeText ("danceData", "Danceability: " + songData.audio_features[0].danceability); 
-				writeText ("acousticData", "Acousticness: " + songData.audio_features[0].acousticness); 
-				writeText ("energyData", "Energy: " + songData.audio_features[0].energy); 
-				writeText ("speechData", "Speechiness: " + songData.audio_features[0].speechiness); 
-				writeText ("liveData", "Liveness: " + songData.audio_features[0].liveness); 
-				writeText ("valenceData", "Valence: " + songData.audio_features[0].valence); 
-				writeText ("loudData", "Loudness: " + songData.audio_features[0].loudness + " lufs"); 
-				writeText ("tempoData", "Tempo: " + songData.audio_features[0].tempo + " bpm"); 
-				writeText ("keyData", "Key:" + songData.audio_features[0].key); 
-				
+   	// 		writeText ("danceData", "Danceability: " + songData.audio_features[0].danceability); 
+				// writeText ("acousticData", "Acousticness: " + songData.audio_features[0].acousticness); 
+				// writeText ("energyData", "Energy: " + songData.audio_features[0].energy); 
+				// writeText ("speechData", "Speechiness: " + songData.audio_features[0].speechiness); 
+				// writeText ("liveData", "Liveness: " + songData.audio_features[0].liveness); 
+				// writeText ("valenceData", "Valence: " + songData.audio_features[0].valence); 
+				// writeText ("loudData", "Loudness: " + songData.audio_features[0].loudness + " lufs"); 
+				// writeText ("tempoData", "Tempo: " + songData.audio_features[0].tempo + " bpm"); 
+				// writeText ("keyData", "Key: " + songData.audio_features[0].key); 
+
 				donutChart1 ('#danceDonut', songData.audio_features[0].danceability, "#009999"); 
 				donutChart2 ('#acousticDonut', songData.audio_features[0].acousticness, "#33CC99"); 
 				donutChart3 ('#energyDonut', songData.audio_features[0].energy, "#CC9999"); 
